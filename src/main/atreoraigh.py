bdd151bc1de3a4b9017fec7d53aec04d26b52781
rmurphy <ross.murphy912@gmail.com>
2018-02-21 16:42:32 +0000

    Changed name of experiment_handler to instruction_handler, added server to handle experiments to pass to instruction_handler.

--- src/main/atreoraigh.py
+++ src/main/atreoraigh.py
@@ -1,19 +1,14 @@
-import json
 import logging
-import experiment_handler
+import subprocess
 from netfilterqueue import NetfilterQueue
+
+import experiment_handler
 from packet_commands import get_payload
 
 
 def setup_logger():
     log_format = '%(asctime)-15s %(levelname)s: %(message)s'
     logging.basicConfig(format=log_format, level=logging.DEBUG)
-
-
-setup_logger()
-
-netfilter_queue = NetfilterQueue()
-experiments = [experiment_handler.ExperimentHandler(56566)]
 
 
 def manage_packet(packet):
@@ -24,10 +19,20 @@ def manage_packet(packet):
             experiment.manage_packet(packet)
 
 
-netfilter_queue.bind(1, manage_packet)
-
-try:
-    print('[*] waiting for data')
-    netfilter_queue.run()
-except KeyboardInterrupt:
-    pass
+if __name__ == '__main__':
+    setup_logger()
+    server = experiment_handler.setup_server()
+    netfilter_queue = NetfilterQueue()
+    experiments = []
+    netfilter_queue.bind(1, manage_packet)
+    try:
+        logging.info('[*] waiting for data')
+        netfilter_queue.run()
+    finally:
+        logging.info('Shutting down experiment server...')
+        server.shutdown()
+        logging.info('Flushing routing tables.')
+        flush_command = ['sudo', 'iptables', '-F']
+        subprocess.Popen(flush_command).communicate()
+        print('Shutting down...')
+        exit(0)
--- src/main/experiment_handler.py
+++ src/main/experiment_handler.py
@@ -1,42 +1,26 @@
-import subprocess
-from packet_commands import *
+import logging
+import socketserver
+import threading
 
 
-class ExperimentHandler:
-    def __init__(self, port):
-        self.port = port
-        self.experiment = 0
-        self.experiment_json = {}
-        self.instructions = {}
-        self.instruction_counter = 1
-        self.route_port()
+class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
+    def handle(self):  # TODO Add functionality send experiments to instruction handler.
+        data = str(self.request.recv(1024), 'ascii')
+        received = "Received: {}".format(data)
+        logging.info(received)
+        response = bytes(received, 'ascii')
+        self.request.sendall(response)
 
-    def manage_packet(self, packet):
-        if self.instructions:
-            self.instructions[self.instruction_counter](packet)
-            self.instruction_counter += 1
-            if self.instruction_counter > len(self.instructions):
-                self.instruction_counter = 1
-        else:
-            logging.info('No instructions for port: {}. Defaulting to accept packet.'.format(self.port))
-            accept_packet(packet)
 
-    def route_port(self):
-        routing_command = ['sudo', 'iptables', '-A', 'INPUT', '-p', 'udp', '--dport', str(self.port), '-j', 'NFQUEUE',
-                           '--queue-num', '1']
-        logging.info('Creating new route for port: {}.'.format(self.port))
-        logging.debug(routing_command)
-        subprocess.Popen(routing_command).communicate()
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
 
-    def build_instructions(self, experiment_json):
-        instructions = {}
 
-        command_map = {
-            "ACCEPT": accept_packet,
-            "DROP": drop_packet
-        }
-
-        for instruction in experiment_json["instructions"]:
-            instructions.update({instruction["step"]: command_map[instruction["instruction"]]})
-
-        self.instructions = instructions
+def setup_server():
+    HOST, PORT = 'localhost', 56566
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    with server:
+        server_thread = threading.Thread(target=server.serve_forever)
+        server_thread.daemon = True
+        server_thread.start()
+    return server

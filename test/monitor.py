6223234fa8648dd1f2c708eae2f91c6d37970f5c
rmurphy <ross.omurchadha@gmail.com>
2018-03-21 17:47:43 +0000

    Monitor for system (bind 2)

---
+++ test/monitor.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python2.7
+from scapy.all import *
+
+from netfilterqueue import NetfilterQueue
+import socket
+
+
+def print_and_accept(pkt):
+    print(get_payload(pkt).show())
+    pkt.accept()
+
+
+def get_payload(packet):
+    return scapy.IP(packet.get_payload())
+
+
+nf_queue = NetfilterQueue()
+nf_queue.bind(2, print_and_accept)
+s = socket.fromfd(nf_queue.get_fd(), socket.AF_UNIX, socket.SOCK_STREAM)
+try:
+    nf_queue.run_socket(s)
+except KeyboardInterrupt:
+    pass
+
+s.close()
+nf_queue.unbind()
